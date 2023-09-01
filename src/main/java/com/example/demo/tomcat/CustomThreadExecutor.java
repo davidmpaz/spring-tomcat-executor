f1b449672b49ea2ef86962d03b8d62d439087249
David Paz <davidmpaz@gmail.com>
2023-09-01 12:24:34 +0200

    Iherit all from StandardThreadExecutor

--- src/main/java/com/example/demo/tomcat/CustomThreadExecutor.java
+++ src/main/java/com/example/demo/tomcat/CustomThreadExecutor.java
@@ -16,18 +16,8 @@
  */
 package com.example.demo.tomcat;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.Executor;
 import org.apache.catalina.LifecycleException;
-import org.apache.catalina.LifecycleState;
-import org.apache.catalina.util.LifecycleMBeanBase;
-import org.apache.tomcat.util.res.StringManager;
-import org.apache.tomcat.util.threads.ResizableExecutor;
-import org.apache.tomcat.util.threads.TaskQueue;
-import org.apache.tomcat.util.threads.TaskThreadFactory;
-import org.apache.tomcat.util.threads.ThreadPoolExecutor;
-
-import java.util.concurrent.TimeUnit;
+import org.apache.catalina.core.StandardThreadExecutor;
 
 /**
  * Copy of class {@link org.apache.catalina.core.StandardThreadExecutor}
@@ -35,71 +25,12 @@ import java.util.concurrent.TimeUnit;
  * Copied, pasted and renamed class just for testing/learning purposes. Only
  * logging sentences are different from original.
  */
-@Slf4j
-public class CustomThreadExecutor extends LifecycleMBeanBase implements Executor, ResizableExecutor {
-
-    protected static final StringManager sm = StringManager.getManager(org.apache.catalina.core.StandardThreadExecutor.class);
-
-    // ---------------------------------------------- Properties
-    /**
-     * Default thread priority
-     */
-    protected int threadPriority = Thread.NORM_PRIORITY;
-
-    /**
-     * Run threads in daemon or non-daemon state
-     */
-    protected boolean daemon = true;
+public class CustomThreadExecutor extends StandardThreadExecutor {
 
     /**
      * Default name prefix for the thread name
      */
-    protected String namePrefix = "tomcat-exec-";
-
-    /**
-     * max number of threads
-     */
-    protected int maxThreads = 200;
-
-    /**
-     * min number of threads
-     */
-    protected int minSpareThreads = 25;
-
-    /**
-     * idle time in milliseconds
-     */
-    protected int maxIdleTime = 60000;
-
-    /**
-     * The executor we use for this component
-     */
-    protected ThreadPoolExecutor executor = null;
-
-    /**
-     * the name of this thread pool
-     */
-    protected String name;
-
-    /**
-     * The maximum number of elements that can queue up before we reject them
-     */
-    protected int maxQueueSize = Integer.MAX_VALUE;
-
-    /**
-     * After a context is stopped, threads in the pool are renewed. To avoid renewing all threads at the same time, this
-     * delay is observed between 2 threads being renewed.
-     */
-    protected long threadRenewalDelay = org.apache.tomcat.util.threads.Constants.DEFAULT_THREAD_RENEWAL_DELAY;
-
-    private TaskQueue taskqueue = null;
-
-    // ---------------------------------------------- Constructors
-    public CustomThreadExecutor() {
-        // empty constructor for the digester
-        log.debug("CustomThreadExecutor created");
-    }
-
+    protected String namePrefix = "custom-tomcat-exec-";
 
     // ---------------------------------------------- Public Methods
 
@@ -112,16 +43,8 @@ public class CustomThreadExecutor extends LifecycleMBeanBase implements Executor
      */
     @Override
     protected void startInternal() throws LifecycleException {
-        log.debug("CustomThreadExecutor.startInternal called");
-
-        taskqueue = new TaskQueue(maxQueueSize);
-        TaskThreadFactory tf = new TaskThreadFactory(namePrefix, daemon, getThreadPriority());
-        executor = new ThreadPoolExecutor(getMinSpareThreads(), getMaxThreads(), maxIdleTime, TimeUnit.MILLISECONDS,
-                taskqueue, tf);
-        executor.setThreadRenewalDelay(threadRenewalDelay);
-        taskqueue.setParent(executor);
-
-        setState(LifecycleState.STARTING);
+        System.out.println("CustomThreadExecutor.startInternal called");
+        super.startInternal();
     }
 
 
@@ -133,176 +56,14 @@ public class CustomThreadExecutor extends LifecycleMBeanBase implements Executor
      */
     @Override
     protected void stopInternal() throws LifecycleException {
-
-        log.debug("CustomThreadExecutor.stopInternal called");
-        setState(LifecycleState.STOPPING);
-        if (executor != null) {
-            executor.shutdownNow();
-        }
-        executor = null;
-        taskqueue = null;
+        super.stopInternal();
+        System.out.println("CustomThreadExecutor.stopInternal called");
     }
 
 
     @Override
     public void execute(Runnable command) {
-        log.debug("CustomThreadExecutor.execute called");
-        if (executor != null) {
-            // Note any RejectedExecutionException due to the use of TaskQueue
-            // will be handled by the o.a.t.u.threads.ThreadPoolExecutor
-            executor.execute(command);
-        } else {
-            throw new IllegalStateException(sm.getString("standardThreadExecutor.notStarted"));
-        }
-    }
-
-    public void contextStopping() {
-        if (executor != null) {
-            executor.contextStopping();
-        }
-    }
-
-    public int getThreadPriority() {
-        return threadPriority;
-    }
-
-    public boolean isDaemon() {
-
-        return daemon;
-    }
-
-    public String getNamePrefix() {
-        return namePrefix;
-    }
-
-    public int getMaxIdleTime() {
-        return maxIdleTime;
-    }
-
-    @Override
-    public int getMaxThreads() {
-        return maxThreads;
-    }
-
-    public int getMinSpareThreads() {
-        return minSpareThreads;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    public void setThreadPriority(int threadPriority) {
-        this.threadPriority = threadPriority;
-    }
-
-    public void setDaemon(boolean daemon) {
-        this.daemon = daemon;
-    }
-
-    public void setNamePrefix(String namePrefix) {
-        this.namePrefix = namePrefix;
-    }
-
-    public void setMaxIdleTime(int maxIdleTime) {
-        this.maxIdleTime = maxIdleTime;
-        if (executor != null) {
-            executor.setKeepAliveTime(maxIdleTime, TimeUnit.MILLISECONDS);
-        }
-    }
-
-    public void setMaxThreads(int maxThreads) {
-        this.maxThreads = maxThreads;
-        if (executor != null) {
-            executor.setMaximumPoolSize(maxThreads);
-        }
-    }
-
-    public void setMinSpareThreads(int minSpareThreads) {
-        this.minSpareThreads = minSpareThreads;
-        if (executor != null) {
-            executor.setCorePoolSize(minSpareThreads);
-        }
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setMaxQueueSize(int size) {
-        this.maxQueueSize = size;
-    }
-
-    public int getMaxQueueSize() {
-        return maxQueueSize;
-    }
-
-    public long getThreadRenewalDelay() {
-        return threadRenewalDelay;
-    }
-
-    public void setThreadRenewalDelay(long threadRenewalDelay) {
-        this.threadRenewalDelay = threadRenewalDelay;
-        if (executor != null) {
-            executor.setThreadRenewalDelay(threadRenewalDelay);
-        }
-    }
-
-    // Statistics from the thread pool
-    @Override
-    public int getActiveCount() {
-        return (executor != null) ? executor.getActiveCount() : 0;
-    }
-
-    public long getCompletedTaskCount() {
-        return (executor != null) ? executor.getCompletedTaskCount() : 0;
-    }
-
-    public int getCorePoolSize() {
-        return (executor != null) ? executor.getCorePoolSize() : 0;
-    }
-
-    public int getLargestPoolSize() {
-        return (executor != null) ? executor.getLargestPoolSize() : 0;
-    }
-
-    @Override
-    public int getPoolSize() {
-        return (executor != null) ? executor.getPoolSize() : 0;
-    }
-
-    public int getQueueSize() {
-        return (executor != null) ? executor.getQueue().size() : -1;
-    }
-
-
-    @Override
-    public boolean resizePool(int corePoolSize, int maximumPoolSize) {
-        if (executor == null) {
-            return false;
-        }
-
-        executor.setCorePoolSize(corePoolSize);
-        executor.setMaximumPoolSize(maximumPoolSize);
-        return true;
-    }
-
-
-    @Override
-    public boolean resizeQueue(int capacity) {
-        return false;
-    }
-
-
-    @Override
-    protected String getDomainInternal() {
-        // No way to navigate to Engine. Needs to have domain set.
-        return null;
-    }
-
-    @Override
-    protected String getObjectNameKeyProperties() {
-        return "type=Executor,name=" + getName();
+        super.execute(command);
+        System.out.println("CustomThreadExecutor.execute called");
     }
 }
